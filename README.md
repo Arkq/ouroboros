5cd084794fa5996211bdfd44c273152a4852ea32
Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>
2015-02-21 19:16:54 +0100

    Initial commit of ouroboros

---
+++ README.md
@@ -0,0 +1,11 @@
+ouroboros - Ongoing Build Assistant
+===================================
+
+
+Instalation
+-----------
+
+	$ autoreconf --install
+	$ mkdir build && cd build
+	$ ../configure
+	$ make && make install
---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+ * ouroboros - main.c
+ * Copyright (c) 2015 Arkadiusz Bokowy
+ *
+ * This file is a part of an ouroboros.
+ *
+ * This projected is licensed under the terms of the MIT license.
+ *
+ */
+
+#if HAVE_CONFIG_H
+#include "../config.h"
+#endif
+
+int main(int argc, char **argv) {
+	return 0;
+}
